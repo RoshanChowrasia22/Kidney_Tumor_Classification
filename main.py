abdb3950a2dee876487e3a05977164742d3cbe40
Roshan Chowrasia <chowrasiar@alum.iisc.ac.in>
2025-11-14 15:37:30 +0530

    Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.cnnClassifier import logger
+from src.cnnClassifier.components.data_ingestion import *
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 
-logger.info("HOLA")
+
+STAGE_NAME="Data Ingestion stage"
+try:
+    logger.info(f"Stage {STAGE_NAME} started")
+    obj=DataIngestionPipeline()
+    obj.main()
+    logger.info(f"Stage {STAGE_NAME} completed" )
+except Exception as e:
+    logger.exception(e)
+    raise e
