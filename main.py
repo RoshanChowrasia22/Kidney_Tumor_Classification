1a6b845d133e24331c5385441f326a6d510da529
Roshan Chowrasia <chowrasiar@alum.iisc.ac.in>
2025-11-14 17:43:32 +0530

    Base model created

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.components.data_ingestion import *
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
-
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 try:
@@ -9,6 +9,17 @@ try:
     obj=DataIngestionPipeline()
     obj.main()
     logger.info(f"Stage {STAGE_NAME} completed" )
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="prepare base Model"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 # CONFIG_FILE_PATH=Path("config/config.yaml")
 CONFIG_FILE_PATH=Path(r"C:\Users\Roshan\Desktop\Kidney_Classification\Kidney_Tumor_Classification\config\config.yaml")
-print(CONFIG_FILE_PATH)
+# print(CONFIG_FILE_PATH)
 PARAMS_FILE_PATH=Path(r"C:\Users\Roshan\Desktop\Kidney_Classification\Kidney_Tumor_Classification\params.yaml")
