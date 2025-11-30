982cc4f3b5dff7fbba41ddba359384da50e781b4
Roshan Chowrasia <chowrasiar@alum.iisc.ac.in>
2025-11-30 09:09:13 +0530

    Model Training Coe done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.cnnClassifier import logger
 from src.cnnClassifier.components.data_ingestion import *
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from src.cnnClassifier.pipeline.stage_03_model_training_pipeline import ModelTrainingPipeline
 STAGE_NAME="Data Ingestion stage"
 try:
     logger.info(f"Stage {STAGE_NAME} started")
@@ -19,6 +19,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
